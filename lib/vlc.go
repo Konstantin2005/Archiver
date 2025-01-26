5f84c11a7599450bdceaaf0d190d6b226c7cf4df
kostya <116232031+ISDUJDDNEN@users.noreply.github.com>
2025-01-26 15:50:27 +1000

    В этой вверсии я реализовл 2 новые Функции
    -добавлен метод `encodeBin` который разбивает строку из Маленкьких и
    Укороченныйх больших англиских симоволов в Bynary string
    -добавлен метод `splitByChunks` который из Bynary string делает Chanks
    заданой длины
    -и начаты писаться тесты для всех фукций

--- lib/vlc.go
+++ lib/vlc.go
@@ -1,22 +1,32 @@
 package lib
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 func Encode(str string) string {
+	str = strings.ToLower(str)
+	bSTR := encodeBin(str)
+	chunks := splitByChunks(bSTR, ChunkSize)
 
+	fmt.Println("chunks:", chunks)
 	return ""
 }
 
+const ChunkSize = 8
+
+type BinaryChunks []BinaryChunk
+type BinaryChunk string
+type encodingTable map[rune]string
+
 // prepareText prepare text to be fit for encode :
-// changes upper case бла бла бла : 1 + lower case letter
+// changes upper case : 1 + lower case letter
 // i.g: My name is Ted -> !my name is !Ted
 func prepareText(str string) string {
-
 	var duf strings.Builder
-
 	for _, ch := range str {
 		if unicode.IsUpper(ch) {
 			duf.WriteRune('!')
@@ -26,4 +36,84 @@ func prepareText(str string) string {
 		}
 	}
 	return duf.String()
+}
+
+// разбивает binary string on binary shanks string with given size,
+// i.g.: '100101010010101010101011' -> '10010101 00101010 10101011'
+func splitByChunks(bStr string, ChunkSize int) BinaryChunks {
+	strLen := utf8.RuneCountInString(bStr)
+	ChunksCount := strLen / ChunkSize
+	if strLen/ChunksCount != 0 {
+		ChunksCount++
+	}
+	res := make(BinaryChunks, 0, ChunksCount)
+
+	var duf strings.Builder
+
+	for i, ch := range bStr {
+		duf.WriteString(string(ch))
+
+		if (i+1)%ChunkSize == 0 {
+			res = append(res, BinaryChunk(duf.String()))
+			duf.Reset()
+		}
+	}
+	if duf.Len() != 0 {
+		lastChunk := duf.String()
+
+		lastChunk += strings.Repeat("0", ChunkSize-len(lastChunk))
+		res = append(res, BinaryChunk(lastChunk))
+	}
+	return res
+}
+
+func encodeBin(str string) string {
+	var duf strings.Builder
+	for _, ch := range str {
+		duf.WriteString(bin(ch))
+	}
+	return duf.String()
+}
+
+func bin(ch rune) string {
+	table := getEncodingTable()
+
+	res, ok := table[ch]
+	if !ok {
+		panic("unknown character %c" + string(ch))
+	}
+	return res
+}
+
+func getEncodingTable() encodingTable {
+	return encodingTable{
+		' ': "11",
+		't': "1001",
+		'n': "10000",
+		's': "0101",
+		'r': "01000",
+		'd': "00101",
+		'!': "001000",
+		'c': "000101",
+		'm': "000011",
+		'g': "0000100",
+		'b': "0000010",
+		'v': "00000001",
+		'k': "0000000001",
+		'q': "000000000001",
+		'e': "101",
+		'o': "10001",
+		'a': "011",
+		'i': "01001",
+		'h': "0011",
+		'l': "001001",
+		'u': "00011",
+		'f': "000100",
+		'p': "0000101",
+		'w': "0000011",
+		'y': "0000001",
+		'j': "000000001",
+		'x': "00000000001",
+		'z': "000000000000",
+	}
 }
--- lib/vlc_test.go
+++ lib/vlc_test.go
@@ -1,6 +1,9 @@
 package lib
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_prepareText(t *testing.T) {
 	type args struct {
@@ -26,6 +29,56 @@ func Test_prepareText(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := prepareText(tt.str); got != tt.want {
 				t.Errorf("prepareText() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_encodeBin(t *testing.T) {
+
+	tests := []struct {
+		name string
+		str  string
+		want string
+	}{
+		{
+			name: "Base Test",
+			str:  "!ted",
+			want: "001000100110100101",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := encodeBin(tt.str); got != tt.want {
+				t.Errorf("encodeBin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_splitByChunks(t *testing.T) {
+	type args struct {
+		bStr      string
+		ChunkSize int
+	}
+	tests := []struct {
+		name string
+		args args
+		want BinaryChunks
+	}{
+		{
+			name: "Base Test",
+			args: args{
+				bStr:      "001000100110100101",
+				ChunkSize: 8,
+			},
+			want: BinaryChunks{"00100010", "01101001", "01000000"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitByChunks(tt.args.bStr, tt.args.ChunkSize); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitByChunks() = %v, want %v", got, tt.want)
 			}
 		})
 	}
